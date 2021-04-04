055064e9df36929d932c36dd2ad88d537bea7449
Dmitry Shveikus <dmitry.shveikus@ecomcharge.com>
2021-04-04 20:00:51 +0300

    Update bootstrap
    came across with situation than in production
    environment JS not compiled well. Also
    bootsrap css does not required.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,6 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -10,6 +9,12 @@ import "channels"
 import "bootstrap"
 import "../stylesheets/application"
 
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+})
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
