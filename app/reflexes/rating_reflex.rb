216143cb142d1b72490d1af94c9e6a219cfcd6f5
Alexandr Sikorsky <44235041+SuperSus@users.noreply.github.com>
2021-04-11 11:37:31 +0200

    Update rating_reflex.rb

--- app/reflexes/rating_reflex.rb
+++ app/reflexes/rating_reflex.rb
@@ -6,13 +6,17 @@ class RatingReflex < ApplicationReflex
   def update_rating
     ActiveRecord::Base.transaction do
       current_movie = Movie.find(element.dataset[:movie_id])
-      rating = Rating.find_or_create_by( { user_id: current_user.id, movie_id: current_movie.id  } ) do |rating|
+      # move this part to separate method as u do for current_movie.update_avg_rating (should be the same abstraction level)
+      # -- 
+      rating = Rating.find_or_create_by(user_id: current_user.id, movie_id: current_movie.id) do |rating|
         rating.value = element.dataset[:value]
       end
 
       unless rating.value.to_s == element.dataset[:value]&.to_s
-        rating.update_attribute :value, element.dataset[:value]
+        # update_attribute skips validations
+        rating.update(value: element.dataset[:value])
       end
+      # --
 
       current_movie.update_avg_rating
     end
