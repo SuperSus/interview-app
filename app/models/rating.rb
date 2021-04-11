85a73c3685c11e75d73d3debc4c14542b833cad2
Alexandr Sikorsky <44235041+SuperSus@users.noreply.github.com>
2021-04-11 10:24:45 +0200

    Update rating.rb

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,13 +1,6 @@
 class Rating < ApplicationRecord
-  #after_save :update_movie_averrage_rating
   belongs_to :movie, dependent: :destroy
   belongs_to :user, dependent: :destroy
   validates_presence_of :movie, :user
   validates_inclusion_of :value, :in => 1..10
-
-  private
-
-  def update_movie_averrage_rating
-    movie.update_rating
-  end
 end
